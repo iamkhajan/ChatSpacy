0088bbf4e12506d60c8e73f348f0b903c846d892
khajanpandey <khajanpandey.hdw@gmail.com>
2020-07-15 16:20:52 -0500

    second commit

--- chat_example.py
+++ chat_example.py
@@ -1,6 +1,8 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 chatbot = ChatBot('Charlie')
 trainer = ListTrainer(chatbot)
 def clearChatbotData():
@@ -10,11 +12,21 @@ def clearChatbotData():
 trainer=ListTrainer(chatbot)
 
 def train_from_text_file():
-	data=open('chattrain.txt').read()
+	data=open('chat_training_data.txt').read()
 	conversations=data.strip().split('\n')
 	trainer.train(conversations)
 
 train_from_text_file()
 response=chatbot.get_response('Who is prime minister of india?')
-print(response)
+
+@app.route("/")
+def home():
+ return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+ userText = request.args.get('msg')
+ return str(chatbot.get_response(userText))
+if __name__ == "__main__":
+ app.run()
+
 
